4d830c63c6ad6f1769e12c6676107e46591b34b7
gagankhalsa <gagankhalsa@gmail.com>
2018-02-01 15:15:31 -0500

    Junit5 example using lambdas in java8

---
+++ src/Junit5/FilteringApplesTest.java
@@ -0,0 +1,50 @@
+package Junit5;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Arrays;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+class FilteringApplesTest {
+	java.util.List<FilteringApples.Apple> greenApplesActual=null;
+	java.util.List<FilteringApples.Apple> greenApplesExpected= null;
+	@BeforeEach
+	void setUp() throws Exception {
+		greenApplesActual=Arrays.asList(new FilteringApples.Apple(80, "green"),
+				new FilteringApples.Apple(180, "Red"),
+				new FilteringApples.Apple(140, "Red"),
+				new FilteringApples.Apple(110, "green"));
+		
+		greenApplesExpected=Arrays.asList(new FilteringApples.Apple(80, "green"),
+				                          new FilteringApples.Apple(80, "green"));
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	@Test
+	void testIsGreenApple() {
+	 FilteringApples.Apple obj=new 	FilteringApples.Apple(90,"green");
+	 FilteringApples.Apple obj1=new 	FilteringApples.Apple(90,"Red");
+	 assertAll("Green apple",
+			 ()->assertTrue(FilteringApples.isGreenApple(obj)),
+	          ()->assertFalse(FilteringApples.isGreenApple(obj1)));
+	 
+	//	fail("Not yet implemented");
+	}
+	@Test
+	void testIsHeavyApple() {
+		 FilteringApples.Apple obj=new 	FilteringApples.Apple(190,"green");
+		 FilteringApples.Apple obj1=new 	FilteringApples.Apple(90,"Red");
+		 assertTrue(FilteringApples.isHeavyApple(obj));
+		 assertFalse(FilteringApples.isHeavyApple(obj1));
+		//fail("Not yet implemented");
+	}
+	@Test
+	void testfilterGreenApples() {
+		assertEquals(greenApplesExpected,FilteringApples.filterGreenApples(greenApplesActual));
+	}
+}
+
