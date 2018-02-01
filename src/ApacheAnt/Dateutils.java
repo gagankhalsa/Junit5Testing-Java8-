5c4faed14bceca3b35eaf1aa1c5cd2f741869eab
gagankhalsa <gagankhalsa@gmail.com>
2018-02-01 16:48:24 -0500

    Example of apache Ant

---
+++ src/ApacheAnt/Dateutils.java
@@ -0,0 +1,15 @@
+package ApacheAnt;
+
+import java.util.Date;
+
+public class Dateutils {
+public static void main(String[] args) {
+	System.out.println(getLocalcurrentDate());
+	
+}
+
+private static Date getLocalcurrentDate() {
+	// TODO Auto-generated method stub
+	return new Date();
+}
+}
